87fa6c4654138132678330f0cf7e43e9402d91fd
Nithin M.K <nithinmkurien@gmail.com>
2024-01-27 17:24:38 +0530

    03-assignment-solution

--- shared.js
+++ shared.js
@@ -1,11 +1,21 @@
-var backdrop = document.querySelector('.backdrop');
-var modal = document.querySelector('.modal');
-var selectPlanButtons = document.querySelectorAll('.plan button');
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButtons = document.querySelectorAll(".plan button");
 
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
-    selectPlanButtons[i].addEventListener('click', function() {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
-    });
-}   
+  selectPlanButtons[i].addEventListener("click", function() {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+}
+
+backdrop.addEventListener("click", closeModal);
+
+modalNoButton.addEventListener("click", closeModal);
+
+function closeModal() {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+}
