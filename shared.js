2380a901bdbf8b90aabf6dae0c9b486522361d59
Nithin M.K <nithinmkurien@gmail.com>
2024-04-29 11:25:04 +0530

    09-finished

--- shared.js
+++ shared.js
@@ -4,6 +4,7 @@ var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
+var ctaButton = document.querySelector(".main-nav__item--cta");
 
 // console.dir(backdrop.style['background-image']);
 
@@ -52,3 +53,15 @@ toggleButton.addEventListener("click", function() {
     backdrop.classList.add("open");
   }, 10);
 });
+
+ctaButton.addEventListener('animationstart', function(event) {
+  console.log('Animation started', event);
+})
+
+ctaButton.addEventListener('animationend', function(event) {
+  console.log('Animation ended', event);
+})
+
+ctaButton.addEventListener('animationiteration', function(event) {
+  console.log('Animation iteration', event);
+})
